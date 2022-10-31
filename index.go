31b95340cf0124838e085f153d755ce311bc1ed5
MinhHamHoc <anhminh.github@gmail.com>
2022-10-31 17:18:30 +0700

    fix err

---
+++ index.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"Crawl_Web/SaveIntoMongo"
+)
+
+var (
+	trTags = regexp.MustCompile(`<tr[^>]*>(?:.|\n)*<\/tr>`)
+	tagA   = regexp.MustCompile(`<\s*a[^>]*>(.*?)<\s*/\s*a>`)
+)
+
+func getStringURL(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	StringURL := string(body)
+	return StringURL
+}
+
+func getTagTrElement(url string) string {
+	div := trTags.FindAllStringSubmatch(url, -1)
+	return div[0][0]
+}
+
+func getTagAElement(fatherElement string) string {
+	trTag := getTagTrElement(fatherElement)
+	tags := tagA.FindAllStringSubmatch(trTag, -1)
+	return tags[0][0]
+}
+
+func getLinkDomain(URL string) string {
+	StringURL := getStringURL(URL)
+	tags := getTagAElement(StringURL)
+	links := strings.Split(tags, "\"")
+	IndexIncludeLinkOnHref := 1
+	return links[IndexIncludeLinkOnHref]
+}
+
+func downloadFromUrl(url string) {
+	tokens := strings.Split(url, "/")
+	file := tokens[len(tokens)-1]
+	fileName := file + ".zip"
+	fmt.Println("Downloading", url, "to", fileName)
+
+	//Check file existence with io.IsExit
+	output, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error while creating: ", err)
+		return
+	}
+	defer output.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	n, err := io.Copy(output, resp.Body)
+	if err != nil {
+		fmt.Println("Error while downloading: ", err)
+		return
+	}
+	fmt.Println(n, "Downloaded")
+
+	unZip(fileName)
+}
+
+func unZip(name string) {
+	fileName := fmt.Sprintf(name)
+	reader, _ := zip.OpenReader(fileName)
+	defer reader.Close()
+
+	for _, file := range reader.File {
+		in, _ := file.Open()
+		defer in.Close()
+
+		year, month, day := time.Now().Date()
+		yearString := strconv.Itoa(year)
+		dayString := strconv.Itoa(day)
+
+		folderName := path.Join(yearString, month.String(), dayString, file.Name)
+
+		dir := path.Dir(folderName)
+		os.MkdirAll(dir, 0777)
+		out, err := os.Create(folderName)
+		if err != nil {
+			fmt.Println("Error while UnZip")
+		}
+		defer out.Close()
+
+		n, err := io.Copy(out, in)
+		if err != nil {
+			fmt.Println("Error while UnZip: ", err)
+		}
+		fmt.Println(n, "Successful Unzip")
+	}
+}
+
+func main() {
+	const URL = "https://www.whoisds.com/newly-registered-domains"
+	link := getLinkDomain(URL)
+	fmt.Println("Link: ", link)
+	resp, err := http.Get(link)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Status: ", resp.Status)
+	if resp.StatusCode != 200 {
+		return
+	}
+
+	downloadFromUrl(link)
+
+	year, month, day := time.Now().Date()
+	pathFile := strconv.Itoa(year) + "/" + month.String() + "/" + strconv.Itoa(day) + "/" + "domain-names.txt"
+	SaveIntoMongo.Saving(pathFile, year, month.String(), day)
+
+}
